cb98ab0ebcce93b15b11deedbe8af81416005f39
Liju-07 <lijusaju03@gmail.com>
2022-10-11 00:03:07 +0530

    angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewsComponent } from './news/news.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule,Routes } from '@angular/router';
+
+const appRoutes:Routes=[
+  {
+    path:"",component:NewsComponent
+  }
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
